3dd6a3dcddf3dffcae2727ad0306368f20fed8e9
Ravi Kumar Nimmi <nimmi.ravikumar@gmail.com>
2016-03-13 01:11:33 +0530

    refactor

--- extension/main.js
+++ extension/main.js
@@ -1,9 +1,4 @@
 
-function airtelDataUsagePage(){
-    var newURL = "https://www.airtel.in/personal/myaccount/telemedia/dsl-usage-details-24h";
-    chrome.tabs.create({ url: newURL });
-}
-
 function showPlanDetails(){
     $.ajax({url: 'http://122.160.230.125:8080/planupdate/',
             success: function(result){
@@ -35,14 +30,55 @@ function setDataBadge(data){
     chrome.browserAction.setBadgeText({text: data});
 }
 
-function onWindowLoad(){
-    showPlanDetails();
+function showCharts(acctId){
+    var date = new Date();
+    var endDate24 = date.toISOString().split('T')[0];
+    date.setDate(date.getDate()-1);
+    var startDate24 = date.toISOString().split('T')[0];
+
+    var endDate = date.toISOString().split('T')[0];
+    date.setDate(date.getDate()-30);
+    var startDate = date.toISOString().split('T')[0];
+
+    params = {
+            acctId: acctId,
+            startDate: startDate24,
+            endDate: endDate24,
+            IsHistoricalRequired: 'N'
+    };
+    getUsageHistory(params, 'container24');
+
+    params = {
+            acctId: acctId,
+            startDate: startDate,
+            endDate: endDate,
+            IsHistoricalRequired: 'Y'
+    };
+    getUsageHistory(params, 'container');
 }
 
-window.onload = onWindowLoad;
+function getUsageHistory(params, div_id) { 
+    var baseUrl   = 'https://www.airtel.in/services/reportserviceusagerecords_V1_1/customerusagedata';
+    $.ajax({url: baseUrl,
+          data: params,
+          success: function(result){
+              if(typeof(result) == "object")
+                  bucketAndDisplay(result, div_id);
+              else
+                  loggedOutMessage();
+          },
+          error: function(jqXHR, textStatus, errorThrown){
+              console.log(errorThrown, textStatus);
+          }
+    });
+}
 
+function airtelLoginPage(){
+    var newURL = "https://www.airtel.in/personal/myaccount/home";
+    chrome.tabs.create({ url: newURL });
+}
 
-function LoggedOutMessage(){
+function loggedOutMessage(){
     $("#container").html("");
     $("#container24").html("");
     var loginHTML ="<span style='color: red;'>Please log in to your broadband account to view past internet usage details below.</span>"+
@@ -50,33 +86,27 @@ function LoggedOutMessage(){
                    "<a class='waves-effect waves-light btn #00bcd4 cyan' id='airtel_page'>Login</a>"+
                    "<br/><br/>";
     $("#login").html(loginHTML);
-    $("#airtel_page").click(airtelDataUsagePage); 
+    $("#airtel_page").click(airtelLoginPage); 
 }
 
-function showCharts(acctId){
-        var date = new Date();
-        var endDate24 = date.toISOString().split('T')[0];
-        date.setDate(date.getDate()-1);
-        var startDate24 = date.toISOString().split('T')[0];
-
-        var endDate = date.toISOString().split('T')[0];
-        date.setDate(date.getDate()-30);
-        var startDate = date.toISOString().split('T')[0];
-        params = {
-                acctId: acctId,
-                startDate: startDate24,
-                endDate: endDate24,
-                IsHistoricalRequired: 'N'
-        };
-        getUrl(params, getDataUsage, LoggedOutMessage);
-
-        params = {
-                acctId: acctId,
-                startDate: startDate,
-                endDate: endDate,
-                IsHistoricalRequired: 'Y'
-        };
-        getUrl(params, getDataUsage, LoggedOutMessage);
+function bucketAndDisplay(response, div_id){
+    usageList = response.GetCustomerDSLUsageResponse.Usage;
+    dataDict = {}
+    for(var i=0;i<usageList.length;i++)
+    {
+        data = parseFloat(usageList[i].ConsumedData);
+        var date = usageList[i].UsageSearchTime.substring(0, 10);
+        if(date in dataDict)
+            dataDict[date] += data;
+        else
+            dataDict[date] = data;
+    }
+    var x=[], y=[];
+    for(var object in dataDict){
+        x.push(object);
+        y.push(dataDict[object]/(1024*1024));
+    }
+    displayChart(x, y, div_id);
 }
 
 function displayChart(x, y, div_id){
@@ -102,42 +132,5 @@ function displayChart(x, y, div_id){
     });
 }
 
+window.onload = showPlanDetails;
 
-function getUrl(params, callback, callbackLogin) { 
-  var baseUrl   = 'https://www.airtel.in/services/reportserviceusagerecords_V1_1/customerusagedata';
-  $.ajax({url: baseUrl,
-          data: params,
-          success: function(result){
-              if(typeof(result) == "object")
-                  callback(result, params.IsHistoricalRequired);
-              else
-                  callbackLogin();
-          },
-          error: function(jqXHR, textStatus, errorThrown){
-              console.log(errorThrown, textStatus);
-          }
-  });
-}
-
-function getDataUsage(response, IsHistoricalRequired){
-    usageList = response.GetCustomerDSLUsageResponse.Usage;
-    dataDict = {}
-    for(var i=0;i<usageList.length;i++)
-    {
-        data = parseFloat(usageList[i].ConsumedData);
-        var date = usageList[i].UsageSearchTime.substring(0, 10);
-        if(date in dataDict)
-            dataDict[date] += data;
-        else
-            dataDict[date] = data;
-    }
-    var x=[], y=[];
-    for(var object in dataDict){
-        x.push(object);
-        y.push(dataDict[object]/(1024*1024));
-    }
-    if(IsHistoricalRequired === 'N')
-        displayChart(x, y, 'container24');
-    else
-        displayChart(x, y, 'container');
-}
